0717465a1240a796d64653773561158c19ef1188
Gero Nootz <gero@nootz.de>
2024-01-19 19:44:27 -0600

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,17 @@
 // https://github.com/adafruit/Adafruit-GFX-Library/commit/80b1bc48cbaac8a33054383124db6d613cbde3c2
 // https://tchapi.github.io/Adafruit-GFX-Font-Customiser/
 
+#define LINE_DELAY 20
+
 void setup()
 {
-  Serial.begin(115200); // Set serial speed to 115200 baud  
+  Serial.begin(115200); // Set serial speed to 115200 baud
 }
 
 void loop()
 {
   // for(int i = -16; i < 79; i++) // entire font
-  for(int i = 0; i < 9; i++) // only numbers
+  for (int i = 0; i < 9; i++) // only numbers
   {
     char ch = i + 48; // ASCII code of '0' is 48
 
@@ -30,26 +32,23 @@ void loop()
       {
         uint16_t i_bit = glyph.width * y + x;
         // Calculate the position in the bitmap array
-        int pos = glyph.bitmapOffset + i_bit / 8; // rounding down the width to the nearest byte
-        // int pos = glyph.bitmapOffset + x / 8 + y * ((glyph.width + 7) / 8); // rounding up the width to the nearest byte
+        int i_byte = glyph.bitmapOffset + i_bit / 8; // rounding down the width to the nearest byte
 
         // Get the byte from the bitmap array
-        uint8_t data = pgm_read_byte(&FreeMonoBold24pt7bBitmaps[pos]);
+        uint8_t data = pgm_read_byte(&FreeMonoBold24pt7bBitmaps[i_byte]);
 
         // Check if the specific pixel is set
         bool pixelSet = data & (0x80 >> (i_bit % 8));
         Serial.print(pixelSet ? '.' : ' ');
       }
       Serial.println();
-      delay(10);
+      delay(LINE_DELAY);
     }
     Serial.println();
-    delay(10);
+    delay(LINE_DELAY);
     Serial.println();
-    delay(10);
+    delay(LINE_DELAY);
     Serial.println();
-    delay(10);
-    
-
+    delay(LINE_DELAY);
   }
 }
